3ddbda3c7d72728f69dc297904049bb5f033277d
Dasha <trofimova.dashas@gmail.com>
2021-08-19 13:27:50 +0300

    added task form with select lesson4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import {Users} from "./components.users.via.select.option/users/Users";
+import Posts from "./components.users.via.select.option/posts/Posts";
+import {useState} from "react";
+import {getPostsByUserId} from "./components.users.via.select.option/post.service";
+import './App.css'
 
 function App() {
-  return (
-    <div>
-      Hello
-    </div>
+
+    let [posts, setPosts] = useState(null)
+
+    const onChangeReturnID = (e) => {
+        let id = e.target.value.split('.')[0]
+        getPostsByUserId(id).then(value => setPosts([...value]))
+    }
+
+    return (
+        <div className={'wrap'}>
+            <Users onChangeReturnID={onChangeReturnID}/>
+            <Posts posts={posts}/>
+        </div>
   );
 }
 
